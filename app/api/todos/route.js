f51edcbf4afd1b23ecf9ea364a63d919226d53bc
marlon nino <mvnino.halcyondigital@gmail.com>
2024-03-20 00:10:24 +0800

    complete api crud

--- app/api/todos/route.js
+++ app/api/todos/route.js
@@ -1,10 +1,27 @@
 import { NextResponse } from "next/server";
 import { connectToDatabase } from "@/utils/database";
 
-export async function GET(request) {
+export const GET = async (request) => {
   const connection = await connectToDatabase();
 
   const [results] = await connection.query("SELECT * FROM tasks");
 
   return NextResponse.json(results);
-}
+};
+
+export const POST = async (request) => {
+  try {
+    const { name, description } = await request.json();
+
+    const connection = await connectToDatabase();
+
+    await connection.query(
+      "INSERT INTO tasks (name, description) VALUES (?, ?)",
+      [name, description]
+    );
+
+    return NextResponse.json("Created!");
+  } catch (error) {
+    return NextResponse.error(error);
+  }
+};
